85e03a1ac4fdfb5befe6e0a6cf28433ae44644cc
Abner Santos <abner.santos@usp.br>
2019-06-19 18:07:21 -0300

    Commented some of the code

--- Assets/Scripts/KanaDatabase.cs
+++ Assets/Scripts/KanaDatabase.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 using System;
 using RotaryHeart.Lib.SerializableDictionary;
 
+/// <summary>
+///     Kana (Japanese alphabets) database. This class stores all the japanese characters and their readings to verify hits/misses from the player.
+///     The actual values of the characters and readings we wanted in the game were filled in Unity by us.
+/// </summary>
 [CreateAssetMenu(fileName = "New KanaDataBase", menuName = "Kana/Database", order = 0)]
-public class KanaDatabase : ScriptableObject
+public class KanaDatabase : ScriptableObject  // The ScriptableObject inheritance makes it possible to store this database in a file
 {
-    public KanaDictionary hiraganaStats = new KanaDictionary();
-    public KanaDictionary katakanaStats = new KanaDictionary();
+    public KanaDictionary hiraganaStats = new KanaDictionary();     // Hiragana is one of the japanese alphabets
+    public KanaDictionary katakanaStats = new KanaDictionary();     // Katakana is also one of the japanese alphabets
 
+    /// <summary>
+    ///     Should the player want to reset this progress.
+    /// </summary>
     public void ResetHitsMisses()
     {
         foreach(var key in hiraganaStats.Keys)
@@ -26,9 +33,19 @@ public class KanaDatabase : ScriptableObject
     }
 
 }
+
+/// <summary>
+///     This is a custom dictionary that inherits from the default dictionary in C# but makes it possible for us to fill in the values in Unity's interface.
+///     Other than that, it is a normal Dictionary with KeyValuePairs, Dictionary[Key] = Value, but the Value here is a custom class witch stores the values we need.static
+/// </summary>
+/// <typeparam name="string"></typeparam>
+/// <typeparam name="ReadingsAndStats"></typeparam>
 [System.Serializable]
 public class KanaDictionary : SerializableDictionaryBase<string, ReadingsAndStats> {    }
 
+/// <summary>
+///     This is the custom class to be used with the dictionary, it stores the roman alphabet counterparts for the japanese characters, how many times that character has appeared and how many times the player got the reading wrong.
+/// </summary>
 [System.Serializable]
 public class ReadingsAndStats
 {
