2e54619809b71297e181d09fd983e537dc150263
HenriqueCamillo <henriquecamillo@usp.br>
2019-06-19 23:40:08 -0300

    Started syllabary progress scene

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -8,8 +8,60 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class Buttons : MonoBehaviour
 {
+    /// <summary>
+    /// Vai para o modo de jogo de indentificação de hiragana
+    /// </summary>
     public void IndentifyHiragana()
     {
 
+    }
+
+    /// <summary>
+    /// Vai para o modo de jogo de escrita de hiragana
+    /// </summary>
+    public void WriteHiragana()
+    {
+
+    }
+
+    /// <summary>
+    /// Vai para o modo de jogo de indentificação de katakana
+    /// </summary>
+    public void IndentifyKatakana()
+    {
+
+    }
+
+    /// <summary>
+    /// Vai para o modo de jogo de escrita de katakana
+    /// </summary>
+    public void WriteKatakana()
+    {
+
+    }
+
+    /// <summary>
+    /// Vai para a tela de mostrar progresso de hiragana
+    /// </summary>
+    public void ViewHiraganaProgress()
+    {
+
+    }
+
+    /// <summary>
+    /// Vai para a tela de mostrar progresso de hiragana
+    /// </summary>
+    public void ViewKatakanaProgress()
+    {
+
+    }
+
+    /// <summary>
+    /// Vai para o menu principal, se já não estiver nele
+    /// </summary>
+    public void Menu()
+    {
+        if (!SceneManager.GetActiveScene().name.Equals("Menu"))
+            SceneManager.LoadScene("Menu");
     }
 }
