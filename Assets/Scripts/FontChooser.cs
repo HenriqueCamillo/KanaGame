f2c7ceade4453b0f1fc020489d269821a75cc5a3
HenriqueCamillo <henriquecamillo@usp.br>
2019-06-21 18:10:42 -0300

    Fixed katakana broken letters problem

---
+++ Assets/Scripts/FontChooser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+/// <summary>
+/// Escolhe a fonte dependendo do alfabeto
+/// </summary>
+public class FontChooser : MonoBehaviour
+{
+    [SerializeField] TMP_FontAsset hiragana, katakana;
+    [SerializeField] TextMeshProUGUI letter;
+
+    /// <summary>
+    ///  Verifica o silabário no game manager e escolhe a fonte
+    /// </summary>
+    void Start()
+    {
+        if (GameManager.instance.alphabet == GameManager.Alphabet.Katakana)
+            letter.font = katakana;
+        else
+            letter.font = hiragana;
+    }
+}
